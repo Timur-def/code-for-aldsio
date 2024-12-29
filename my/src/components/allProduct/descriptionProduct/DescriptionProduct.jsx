d4d1cce8a1286a532d26a38eee9afe265bf227e1
Timur-def <timuraktan47@gmail.com>
2024-12-29 22:38:56 +0300

    verse1

--- my/src/components/allProduct/descriptionProduct/DescriptionProduct.jsx
+++ my/src/components/allProduct/descriptionProduct/DescriptionProduct.jsx
@@ -2,16 +2,20 @@ import { useLocation } from "react-router-dom";
 import "./DescriptionProduct.scss";
 export default function DescriptionProduct() {
   const location = useLocation();
-  const {product} = location.state || {}; // Получаем описание из state
+  const {data} = location.state || {}; // Получаем описание из state
+  if (!data) {
+    console.log(location.state);
+    return <p>Descr invalid</p>
+  }
 
   return (
     <div className="cardDescription">
       <h2>Описание продукта</h2>
-      <img src={product.image} className="cardDescription__image"/>
+      <img src={data.image} className="cardDescription__image"/>
       <div className="cardDescription__bottomBlock">
-      <h2 className="cardDescription__price">Цена: {product.price}₽</h2>
-      {product.description ? (
-        <h3 className="cardDescription__Descr">{product.description}</h3>
+      <h2 className="cardDescription__price">Цена: {data.price}₽</h2>
+      {data.description ? (
+        <h3 className="cardDescription__Descr">{data.description}</h3>
       ) : (
         <p>Описание не доступно</p>
       )}
